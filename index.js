e222993bc8b5b35b690a54e8f6ddbd7d9cade6ad
ryuhojin <interarea@naver.com>
2023-06-09 17:53:55 +0900

    function this bind

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 class Bframe {
   constructor(state) {
+    this.syncDom = this.syncDom.bind(this);
     this.data = new Proxy(state.data, {
-      set: function (target, key, value) {
+      set: (target, key, value) => {
         target[key] = value;
-        let model = document.querySelectorAll("[b-model]");
-        model.forEach((el) => {
-          let key = el.getAttribute("b-model");
-          if (this.data[key] != null) {
-            el.value = this.data[key];
-          }
-        });
+        this.syncDom();
         return true;
       },
     });
     this.initModel(this.data);
   }
+
+  syncDom() {
+    let model = document.querySelectorAll("[b-model]");
+    model.forEach((el) => {
+      let key = el.getAttribute("b-model");
+      if (this.data[key] != null) {
+        el.value = this.data[key];
+      }
+    });
+  }
+
   initModel(data) {
     let modelElements = document.querySelectorAll("[b-model]");
     modelElements.forEach((el) => {
