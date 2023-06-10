b04f8d701573e4cf264c296cc3499fb93e6e5614
ryuhojin <interarea@naver.com>
2023-06-11 00:11:41 +0900

    more like vue updated

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
 class Bframe {
-  constructor(state) {
-    this.syncDom = this.syncDom.bind(this);
-    this.data = new Proxy(state.data, {
-      set: (target, key, value) => {
-        target[key] = value;
-        this.syncDom();
-        return true;
-      },
-    });
-    this.initModel(this.data);
+  constructor(options) {
+    this.$options = options;
+    this.$el = document.querySelector(options.el);
+    this.$data = new Proxy(options.data, this.getHandler());
+    this.$methods = this.bindMethods(options.methods, this);
+    this.$mounted = options.mounted;
+    this.$nextTickQueue = [];
+    this.initializeDOM();
+    this.runMounted();
   }
 
-  syncDom() {
-    let model = document.querySelectorAll("[b-model]");
-    model.forEach((el) => {
-      let key = el.getAttribute("b-model");
-      if (this.data[key] != null) {
-        el.value = this.data[key];
+  getHandler() {
+    return {
+      get: (target, key, receiver) => Reflect.get(target, key, receiver),
+      set: (target, key, value, receiver) => {
+        const result = Reflect.set(target, key, value, receiver);
+        this.updateDOM(key);
+        return result;
+      }
+    };
+  }
+
+  bindMethods(methods, context) {
+    let boundMethods = {};
+    for (let key in methods) {
+      boundMethods[key] = methods[key].bind(context);
+    }
+    return boundMethods;
+  }
+
+  runMounted() {
+    if (this.$mounted) {
+      this.$mounted.call(this);
+    }
+  }
+
+  nextTick(callback) {
+    this.$nextTickQueue.push(callback);
+    Promise.resolve().then(() => {
+      while (this.$nextTickQueue.length > 0) {
+        const nextTickCallback = this.$nextTickQueue.shift();
+        nextTickCallback.call(this);
       }
     });
   }
 
-  initModel(data) {
-    let modelElements = document.querySelectorAll("[b-model]");
-    modelElements.forEach((el) => {
-      let key = el.getAttribute("b-model");
-      el.addEventListener("input", function (e) {
-        data[key] = e.target.value;
+  initializeDOM() {
+    const bindings = Array.from(this.$el.querySelectorAll('[b-model]'));
+    bindings.forEach(element => {
+      const key = element.getAttribute('b-model');
+      this.updateElement(element, this.$data[key]);
+      element.addEventListener('input', e => {
+        this.$data[key] = e.target.value;
       });
     });
+  }
+
+  updateDOM(key) {
+    const bindings = Array.from(this.$el.querySelectorAll(`[b-model=${key}]`));
+    bindings.forEach(element => {
+      this.updateElement(element, this.$data[key]);
+    });
+  }
+
+  updateElement(element, value) {
+    if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
+      element.value = value;
+    } else {
+      element.textContent = value;
+    }
   }
 }
